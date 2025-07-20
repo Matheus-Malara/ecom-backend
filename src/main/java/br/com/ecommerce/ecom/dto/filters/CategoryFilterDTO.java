e2b63da108d54f740d4ef242707c1664a243df49
Matheus-Malara <matheus_malara@live.com>
2025-07-19 22:21:36 -0300

    Remove minimum size restriction on `name` in `CategoryFilterDTO`.

--- src/main/java/br/com/ecommerce/ecom/dto/filters/CategoryFilterDTO.java
+++ src/main/java/br/com/ecommerce/ecom/dto/filters/CategoryFilterDTO.java
@@ -1,7 +1,6 @@
 package br.com.ecommerce.ecom.dto.filters;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +12,6 @@ public class CategoryFilterDTO {
     @Schema(description = "Filter by active status", example = "true")
     private Boolean active;
 
-    @Size(min = 2, message = "Search term must have at least 2 characters")
     @Schema(description = "Partial name filter", example = "supp")
     private String name;
 }
