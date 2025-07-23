d53d7de69f29f9a7c7a16a7687827c7fb811a624
Matheus-Malara <matheus_malara@live.com>
2025-07-22 21:29:37 -0300

    Remove `@Email` validation from `UpdateUserStatusRequest` DTO

--- src/main/java/br/com/ecommerce/ecom/dto/requests/UpdateUserStatusRequest.java
+++ src/main/java/br/com/ecommerce/ecom/dto/requests/UpdateUserStatusRequest.java
@@ -1,11 +1,8 @@
 package br.com.ecommerce.ecom.dto.requests;
 
-import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class UpdateUserStatusRequest {
-    @Email
-    private String email;
     private boolean active;
 }
